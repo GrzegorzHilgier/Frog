c8061dbdc8ddbd1d48093a5e14edc825419866e2
Grzegorz Hilgier <g.hilgier@gmail.com>
2019-03-16 11:16:04 +0100

    add drawableobject utility

--- Frog/Models/Player.cs
+++ Frog/Models/Player.cs
@@ -3,41 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Frog.Utilities;
 
 namespace Frog.Models
 {
-    class Player:ObservableObject
+    class Player: DrawableObject
     {
+        public ushort Lives { get; private set; }
+        public int Score { get; private set; } = 0;
+
         public Player(ushort lives, int xcoord, int ycoord, int size)
         {
             Lives = lives;
             Xcoord = xcoord;
-            Ycoord = ycoord;
-            Size = size;
+            Ycoord = ycoord; 
         }
-
-        public ushort Lives { get; private set; }
-        public int Score { get; private set; } = 0;
-        private int xcoord;
-        public int Xcoord
-        {
-            get => xcoord;
-            set
-            {
-                xcoord = value;
-                RaisePropertyChangedEvent("Xcoord");
-            }
-        }
-        private int ycoord;
-        public int Ycoord
-        {
-            get => ycoord;
-            set
-            {
-                ycoord = value;
-                RaisePropertyChangedEvent("Ycoord");
-            }
-        }
-        public int Size { get; private set; }
     }
 }
---
+++ Frog/Utilities/DrawableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Frog.Utilities
+{
+    class DrawableObject
+    {
+        public int Xcoord { get; set; }
+        public int Ycoord { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+    }
+}
